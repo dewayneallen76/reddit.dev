83772f12a3ffe9f81526adf938c642663d8b5922
Dewayne Allen <dewayneallen76@gmail.com>
2016-10-10 17:28:38 -0500

    took the add route that we created earlier in exercises and refactored to use blade templating to display the total

---
+++ resources/views/add.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.master')
+
+@section('top-script')
+	<title>Add</title>
+@stop
+
+@section('content')
+	<h1>Add</h1>
+	<h2>First Number: {{ $a }}</h2>
+	<h2>Second Number: {{ $b }}</h2>
+	<h2>Total: {{ $total }}</h2>
+@stop
