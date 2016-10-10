b971b41b662996006376d9d30db7fad17b887fb4
Dewayne Allen <dewayneallen76@gmail.com>
2016-10-10 14:33:32 -0500

    Create a route at the path /add that takes two parametes that are numbers and returns the sum of the numbers.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -22,4 +22,11 @@ Route::get('/sayhello/{name?}', function($name = 'Kristy') {
 Route::get('/uppercase/{string}', function($string) {
 	return (strtoupper($string));
 });
-
+// Create a route at the path /increment that takes a parameter that is a number and returns the number plus one.
+Route::get('/increment/{number}', function($number) {
+	return $number + 1;
+});
+// Create a route at the path /add that takes two parametes that are numbers and returns the sum of the numbers.
+Route::get('/add/{a}/{b}', function($a,$b) {
+	return $a + $b;
+});
