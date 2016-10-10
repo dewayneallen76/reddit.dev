54f644c3b287c8535e21975220732960baa53cbf
Dewayne Allen <dewayneallen76@gmail.com>
2016-10-10 17:11:13 -0500

    Create a new view named uppercase.blade.php and have this view extend master. Have your uppercase route return the uppercase view. Pass the word entered in the route, as well as the word uppercased as an associative array to the view. In the view, use blade syntax to display the original word and the uppercased version.

---
+++ resources/views/uppercase.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+
+@section('top-script')
+	<title>Uppercase Exercise</title>
+@stop
+
+@section('content')
+	<h1>Uppercase Exercise</h1>
+	<h2>You entered: {{ $string }}</h2>
+	<h2>Uppercased: {{ $upperCase }}</h2>
+@stop
