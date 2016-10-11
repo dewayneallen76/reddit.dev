278db5cad954128e83cd9750ebf5e112ff79926e
Dewayne Allen <dewayneallen76@gmail.com>
2016-10-11 11:23:14 -0500

    added dice blade file for roll dice exercise

---
+++ resources/views/dice.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.bootstrap')
+
+@section('title', 'Roll Some Dice!')
+
+@section('content')
+    <h1>Let's see what we rolled...</h1>
+    @foreach($rolls as $roll)
+        <p>
+            {{ $roll }}
+            @if($guess == $roll)
+                You Got It!
+            @endif
+        </p>
+        <hr>
+    @endforeach
+    <h2>Make another guess</h2>
+    <ul>
+    @for($i = 1; $i <= 6; $i ++)
+        <li>
+            <a href="{{ action ('HomeController@rollDice') }}">
+                Guess {{ $i }}
+            </a>
+        </li>
+    @endfor
+    </ul>
+@stop
