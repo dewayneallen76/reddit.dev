ee79839fabcdd0f35dd3a032b22b417595b49e10
Dewayne Allen <dewayneallen76@gmail.com>
2016-10-11 12:05:17 -0500

    tested partials that i created

--- resources/views/layouts/bootstrap.blade.php
+++ resources/views/layouts/bootstrap.blade.php
@@ -4,13 +4,14 @@
     <meta charset="UTF-8">
         @yield('top-script')
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+    <title>@yield('title')</title>
 </head>
 <body>
-
+	{{-- @include('partials.navbar') --}}
     <div class="container">
         @yield('content')
     </div>
-    
+    {{-- @include('partials.footer') --}}
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 </body>
 </html>
