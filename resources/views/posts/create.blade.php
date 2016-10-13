d5ed9bbb5d8724a6536bc56032f8b247638f6e50
Dewayne Allen <dewayneallen76@gmail.com>
2016-10-13 13:22:14 -0500

    added if statements for each title, url, content, to show error if not valid

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -3,11 +3,27 @@
 @section('title', 'Create Post')
 
 @section('content')
+
 	<form class="form" method="POST" action="{{ action('PostsController@store') }}">
 		{!! csrf_field() !!}
 		Title: <input class="form-control" type="text" name="title" value="{{ old('title') }}">
+		@if($errors->has('title'))
+            <div class="alert alert-danger">
+                {{ $errors->first('title') }}
+            </div>
+        @endif
 		URL: <input class="form-control" type="text" name="url" value="{{ old('url') }}">
+		@if($errors->has('url'))
+			<div class="alert alert-danger">
+				{{ $errors->first('url') }}
+			</div>
+		@endif
 		Content: <textarea class="form-control" type="text" name="content">{{ old('content') }}</textarea>
+		@if($errors->has('content'))
+                <div class="alert alert-danger">
+                    {{ $errors->first('content') }}
+                </div>
+        @endif
 		<br>
 		<button class="btn-success btn" type="submit">Submit New Post</button>
 	</form>
