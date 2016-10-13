8cf667f75acbf992b52f306664952af86c1a38f1
Dewayne Allen <dewayneallen76@gmail.com>
2016-10-13 14:03:42 -0500

    deactivated delete button

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -27,7 +27,7 @@
                 <p class="lead">{{ $post->content }}</p>
                 <hr>
                 <a href="/posts/{{ $post->id }}/edit" class="btn btn-primary btn-lg active" method="POST" role="button">Edit Post</a>
-                <a href="/posts/{{ $post->id }}/delete" class="btn btn-danger btn-lg active" method="POST" role="button">Delete Post</a>
+                <a href="/posts/{{ $post->id }}/delete" class="btn btn-danger btn-lg active" role="button">Delete Post</a>
             </div> 
         </div>
     </div> 
