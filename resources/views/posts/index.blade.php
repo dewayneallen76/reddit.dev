9f73cfa13ebaf0f564c37a2b4a1362c0527bbd76
Dewayne Allen <dewayneallen76@gmail.com>
2016-10-13 10:14:48 -0500

    updated anchor tag link to go to the post id

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,23 +3,23 @@
 @section('title', 'Index Posts')
 
 @section('content')
-	<div class="container-fluid">
-	<table class="table table-bordered">
-  		<tr>	
-  			<th>ID</th>
-  			<th>Title</th>
-  			<th>URL</th>
-  			<th>Content</th>
-  		</tr>
-  		@foreach($posts as $post)
-  		<tr>
-  			<td>{{ $post->id }}</td>
-  			<td>{{ $post->title }}</td>
-  			<td><a href="{{ $post->url }}" target="_blank">{{ $post->url }}</a></td>
-  			<td>{{ $post->content }}</td>
-  		</tr>	
-  		@endforeach
-	</table>
-	</div>
+    <div class="container-fluid">
+    <table class="table table-bordered">
+      <tr>  
+        <th>ID</th>
+        <th>Title</th>
+        <th>URL</th>
+        <th>Content</th>
+      </tr>
+      @foreach($posts as $post)
+      <tr>
+        <td>{{ $post->id }}</td>
+        <td>{{ $post->title }}</td>
+        <td><a href="{{ $post->id }}" target="_blank">{{ $post->url }}</a></td>
+        <td>{{ $post->content }}</td>
+      </tr> 
+      @endforeach
+  </table>
+  </div>
 @stop
 
