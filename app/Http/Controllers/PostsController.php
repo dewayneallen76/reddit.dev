c807d3d633dc04deb5533fa045206329ef01980b
Dewayne Allen <dewayneallen76@gmail.com>
2016-10-17 10:19:40 -0500

    added use statement to use base model

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use App\Models\BaseModel;
 use App\Models\Post;
+use App\Models\BaseModel;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
