da98db1624b29ebd07ff6c6bec57875034dcb0a2
Dewayne Allen <dewayneallen76@gmail.com>
2016-10-18 10:37:24 -0500

    corrected foreach statement to show posts by user on the user show page

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -4,8 +4,9 @@
 	<div class="container">
         <div class="row">
      		<div class = "col-sm-12">
-			<h1>{{ $users->name }}</h1>
-			<h1>{{ $users->email }}</h1>
+			<h3>{{ $users->name }}</h3>
+			<h3>{{ $users->email }}</h3>
+            <h3>Posts:</h3>
      	</div>
             <div class="col-lg-8">
             	<table class="table table-hover">
@@ -15,20 +16,19 @@
             				<th>Title</th>
             				<th>URL</th>
             				<th>Content</th>
-            				<th>Owner</th>
             				<th></th>
             			</tr>
             		</thead>
             		<tbody>
-            			{{-- @foreach($users->posts as $user->$post)
+            			@foreach($users->posts as $post)
             			<tr>
-            				<td>{{ $user->post->id }}</td>
-            				<td>{{ $user->post->title }}</td>
-            				<td><a href="{{ $user->post->url }}">{{ $user->post->url }}</a></td>
-            				<td>{{ $user->post->content }}</td>
-            				<td>{{ $user->post->name }}</td>
+            				<td>{{ $post->id }}</td>
+            				<td>{{ $post->title }}</td>
+            				<td><a href="{{ $post->url }}">{{ $post->url }}</a></td>
+            				<td>{{ $post->content }}</td>
+            				<td>{{ $post->name }}</td>
             			</tr>
-						@endforeach --}}
+						@endforeach
             		</tbody>
             	</table>
     		</div>
