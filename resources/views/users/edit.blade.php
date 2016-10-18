a554775854444f8d25383e7934e93e84c29cb216
Dewayne Allen <dewayneallen76@gmail.com>
2016-10-18 15:45:04 -0500

    added input for confirm password

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -3,13 +3,30 @@
 @section('title', 'Edit Post')
 
 @section('content')
-	<form class="form" method="POST" action="{{ action('UsersController@update', $users->id) }}">
-		{!! csrf_field() !!}
-		{!! method_field('PUT') !!}
-		Name: <input class="form-control" type="text" name="name" value="{{ (old('name') ==null) ? $users->name : old('name') }}">
-		Email: <input class="form-control" type="text" name="email" value="{{ (old('email') ==null) ? $users->email : old('email') }}">
-		Password: <input class="form-control" type="text" name="password" value="{{ (old('password') ==null) ? $users->password : old('password') }}">
-		<br>
-		<button class="btn-primary btn" type="Submit">Edit User <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></button>
-	</form>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-lg-6">
+			<form class="form" method="POST" action="{{ action('UsersController@update', $users->id) }}">
+				{!! csrf_field() !!}
+				{!! method_field('PUT') !!}
+				Name: <input class="form-control" type="text" name="name" value="{{ (old('name') ==null) ? $users->		name : old('name') }}">
+				Email: <input class="form-control" type="text" name="email" value="{{ (old('email') ==null) ? $users->		email : old('email') }}">
+				New Password: <input class="form-control" type="password" name="password">
+				@if($errors->has('password'))
+                <div class="alert alert-danger">
+                    {{ $errors->first('password') }}
+                </div>
+       	 		@endif
+				Confirm Password: <input class="form-control" type="password" name="confirm_password">
+				<br>
+				@if($errors->has('confirm_password'))
+                <div class="alert alert-danger">
+                    {{ $errors->first('confirm_password') }}
+                </div>
+        		@endif
+				<button class="btn-primary btn" type="Submit">Edit User <span class="glyphicon glyphicon-pencil" 		aria-hidden="true"></span></button>
+			</form>
+		</div>
+	</div>
+</div>
 @stop 
