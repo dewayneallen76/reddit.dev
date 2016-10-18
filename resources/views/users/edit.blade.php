45c71e5248e0fbae4aa8b2ee43204b19f1ba6330
Dewayne Allen <dewayneallen76@gmail.com>
2016-10-18 13:40:43 -0500

    added edit button to allow logged in user to edit their info

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -3,13 +3,13 @@
 @section('title', 'Edit Post')
 
 @section('content')
-	<form class="form" method="POST" action="{{ action('UsersController@update', $user->id) }}">
+	<form class="form" method="POST" action="{{ action('UsersController@update', $users->id) }}">
 		{!! csrf_field() !!}
 		{!! method_field('PUT') !!}
-		Name: <input class="form-control" type="text" name="name" value="{{ (old('name') ==null) ? $user->name : old('name') }}">
-		Email: <input class="form-control" type="text" name="email" value="{{ (old('email') ==null) ? $user->email : old('email') }}">
-		Password: <input class="form-control" type="text" name="password" value="{{ (old('password') ==null) ? $user->password : old('password') }}">
+		Name: <input class="form-control" type="text" name="name" value="{{ (old('name') ==null) ? $users->name : old('name') }}">
+		Email: <input class="form-control" type="text" name="email" value="{{ (old('email') ==null) ? $users->email : old('email') }}">
+		Password: <input class="form-control" type="text" name="password" value="{{ (old('password') ==null) ? $users->password : old('password') }}">
 		<br>
-		<button class="btn-primary btn" type="Submit">Edit Post <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></button>
+		<button class="btn-primary btn" type="Submit">Edit User <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></button>
 	</form>
 @stop 
