43bb42df7b0a318a2c4ad432d17f9ee71e5f33f6
Dewayne Allen <dewayneallen76@gmail.com>
2016-10-20 10:01:00 -0500

    changed name from reddit to dreddit

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,7 +6,7 @@
   <div class="container-fluid">
     <div class="row">
             <div class="col-lg-12">
-                <h1 class="page-header">Reddit.dev
+                <h1 class="page-header">Dreddit.dev
                     <small>All Posts</small>
                 </h1>
             </div>
