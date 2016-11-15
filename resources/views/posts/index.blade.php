fcfcfbdd81b59f884093a3d17c290ac97fad40ea
Dewayne Allen <dewayneallen76@gmail.com>
2016-11-15 11:42:38 -0600

    EDITED TO REMOVE .DEV FROM DREDDIT ON POSTS INDEX AND ALSO ON THE NAVBAR

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,7 +6,7 @@
   <div class="container-fluid">
     <div class="row">
             <div class="col-lg-12">
-                <h1 class="page-header">Dreddit.dev
+                <h1 class="page-header">Dreddit
                     <small>All Posts</small>
                 </h1>
             </div>
