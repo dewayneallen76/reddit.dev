48ee7e90c854beacd1a96b14c1ee33089415bf1a
Dewayne Allen <dewayneallen76@gmail.com>
2017-04-26 11:54:09 -0500

    updated commented out section for delete user

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -25,13 +25,13 @@
                 </div>
         		@endif
 				<button class="btn-primary btn" type="Submit">Edit User <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></button>
-        		{{-- DELETE USER BUTTON, NON-FUNCTIONAL AT THIS TIME --}}
-				{{-- <form class="form" method="POST" action="{{ action('UsersController@destroy', $users->id) }}">
-				{!! csrf_field() !!}
-				{!! method_field('DELETE') !!}
-				<button class="btn-primary btn" type="Submit">Delete User <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></button> --}}
+        		<!-- DELETE USER BUTTON, NON-FUNCTIONAL AT THIS TIME -->
+				 <!-- <form class="form" method="POST" action="{{ action('UsersController@destroy', $users->id) }}"> -->
+				<!-- {!! csrf_field() !!} -->
+				<!-- {!! method_field('DELETE') !!} -->
+				<!-- <button class="btn-primary btn" type="Submit">Delete User <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></button> -->
 			</form>
 		</div>
 	</div>
 </div>
-@stop 
+@stop
